c7248442dc5ae5a6536a97fc673e964d8e134331
JiGuoDing <485204300@qq.com>
2025-04-29 12:36:54 +0800

    Created the framework of BooksCooccurrence. @HP.

---
+++ src/main/java/bookSimilarity/BooksCooccurrenceDriver.java
@@ -0,0 +1,31 @@
+package bookSimilarity;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class BooksCooccurrenceDriver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "BooksCooccurrence");
+
+        job.setNumReduceTasks(3);
+        job.setJarByClass(BooksCooccurrenceDriver.class);
+        job.setMapperClass(BooksCooccurrenceMapper.class);
+        job.setReducerClass(BooksCooccurrenceReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ src/main/java/bookSimilarity/BooksCooccurrenceReducer.java
@@ -0,0 +1,13 @@
+package bookSimilarity;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class BooksCooccurrenceReducer extends Reducer<Text, Text, Text, Text> {
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+
+    }
+}
